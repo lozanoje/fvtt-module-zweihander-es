28e8e89af2d36d04f7dc850c70cc375be0160fa4
José Lozano <lozanoje@gmail.com>
2024-12-23 17:11:13 +0100

    Before migrating professions

---
+++ scripts/fvtt-Macro-actor-skills-updater-(zweihander).js
@@ -0,0 +1,45 @@
+//
+// Actor items updater (zweihander) v0.2
+// By Viriato139ac
+//
+
+const macroName = "Actor preferred skills updater";
+const macroVersion = "0.2";
+const macroImage = "icons/sundries/books/book-tooled-blue-yellow.webp";
+
+for (let a of Array.from(game.actors)) {
+  updateActorItems(a)
+}
+ui.notifications.info('Finished updating');
+
+async function updateActorItems(myactor) {
+
+  const worldLanguage = game.settings.get("core", "language");
+  
+    if (worldLanguage !== "es") {
+    ui.notifications.error(`No spanish`);
+    return;
+  }
+
+  let updates = {
+	  "flags.zweihander.actorConfig.parrySkills": [
+          "A simples CaC",
+          "A marciales CaC",
+          "Ardides",
+          "Carisma",
+          "Encantamiento"
+        ],
+	  "flags.zweihander.actorConfig.dodgeSkills": [
+          "Coordinación",
+          "Ardides",
+          "Conducción",
+          "Equitación"
+        ],
+	  "flags.zweihander.actorConfig.magickSkills": [
+          "Encantamiento",
+          "Folclore"
+        ]
+	  }
+await myactor.update(updates);
+
+}
---
+++ scripts/fvtt-Macro-professions-uuid-updater-(zweihander).js
@@ -0,0 +1,66 @@
+//
+// Professions uuid updater (zweihander) v0.1
+// By Rex
+//
+
+const macroName = "Professions uuid updater";
+const macroVersion = "0.1";
+const macroImage = "icons/tools/fasteners/bolt-brass-yellow.webp";
+
+const professionsPack = game.packs.get("fvtt-module-zweihander-es.zh-professions-es");
+const professionsList = await professionsPack.getDocuments();
+
+const migrateProfessionalTrait = async (p, log) => {
+  let professionalTraitName = p.system.professionalTrait.name;
+
+  const professionalTrait = await globalThis.findItemWorldWide(
+    "trait",
+    professionalTraitName
+  );
+
+  log ? console.log(`Migrating Professional Trait for ${p.name}:`, professionalTrait.uuid) : await p.update({ ['system.professionalTrait.uuid']: professionalTrait.uuid });
+};
+
+const migrateSpecialTrait = async (p, log) => {
+  let specialTraitName = p.system.specialTrait.name;
+
+  if (specialTraitName === '') return;
+
+  const specialTrait = await globalThis.findItemWorldWide(
+    "trait",
+    specialTraitName
+  );
+
+  log ? console.log(`Migrating Special Trait for ${p.name}:`, specialTrait.uuid) : await p.update({ ['system.specialTrait.uuid']: specialTrait.uuid });
+};
+
+const migrateDrawback = async (p, log) => {
+  
+  let drawbackName = p.system.drawback.name;
+
+  if (drawbackName === '') return;
+
+  const drawback = await globalThis.findItemWorldWide(
+    "drawback",
+    drawbackName
+  );
+
+  log ? console.log(`Migrating Drawback for ${p.name}:`, drawback.name) : await p.update({ ['system.drawback.uuid']: drawback.uuid });
+};
+
+const migrateTalents = async (p, log) => {
+  let talentList = p.system.talents;
+
+  let updatedTalentList = (await findItemsWorldWide('talent', talentList.map((t) => t.name))).flat().map((t, i) => ({...talentList[i], uuid: t.uuid}));
+
+  log ? console.log(`Migrating Talents for ${p.name}:`, updatedTalentList) : await p.update({ ['system.talents']: updatedTalentList });
+};
+
+const log = true;
+
+professionsList.forEach(async (p) => {
+  await migrateProfessionalTrait(p, log);
+  await migrateSpecialTrait(p, log);
+  await migrateDrawback(p, log);
+  await migrateTalents(p, log);
+});
